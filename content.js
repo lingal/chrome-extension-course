37bd1ce338a62a5c4ec2db758af4dcf72e8ccde9
Lingal <galumbauskasl@gmail.com>
2022-11-08 08:05:07 +0200

    storage feature added to keep user's selection between rendering

--- content.js
+++ content.js
@@ -2,11 +2,36 @@ window.onload = () => {
   const button = document.createElement ('button');
   button.id = 'darkModeButton';
   button.innerText = 'Dark Mode';
-  document.querySelector ('#end').prepend (button);
+  const input = document.createElement ('input');
+  input.type = 'checkbox';
+  input.id = 'darkModeSetting';
+  document.querySelector ('#end').prepend (button, input, 'Auto apply?');
   button.addEventListener ('click', () => handleDarkMode ());
+  input.addEventListener ('click', () => storeSetting ());
+
+  checkSetting ();
+};
+
+const checkSetting = () => {
+  chrome.storage.local.get (['enabled'], result => {
+    const isEnabled = result.enabled;
+
+    console.log(isEnabled);
+    
+    document.getElementById ('darkModeSetting').checked = isEnabled;
+    isEnabled && handleDarkMode();
+  });
 };
 
 const handleDarkMode = () => {
   const container = document.getElementsByTagName ('ytd-app')[0];
   container.style.background = '#000000';
+};
+
+const storeSetting = () => {
+  const isEnabled = document.getElementById ('darkModeSetting').checked;
+  const setting = {
+    enabled: isEnabled,
+  };
+  chrome.storage.local.set (setting);
 };
