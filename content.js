7d013108897a0c392209e8e6944caec2a30ece2a
Lingal <galumbauskasl@gmail.com>
2022-11-08 03:47:19 +0200

    added btn to enable dark mode

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 window.onload = () => {
   const button = document.createElement ('button');
   button.id = 'darkModeButton';
+  button.innerText = 'Dark Mode';
   document.querySelector ('#end').prepend (button);
+  button.addEventListener ('click', () => handleDarkMode ());
+};
+
+const handleDarkMode = () => {
+  const container = document.getElementsByTagName ('ytd-app')[0];
+  container.style.background = '#000000';
 };
